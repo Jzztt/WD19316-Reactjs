154e1e88eb91c458b10dea5e2cf025f89897b8bd
Jettsyy <teazyyy2512@gmail.com>
2025-04-03 17:57:18 +0700

    feat: Register functional

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,27 +1,81 @@
+import { useMutation } from "@tanstack/react-query";
 import { Button, Card, Form, Input } from "antd";
+import axios from "axios";
 import React from "react";
+import { useNavigate } from "react-router";
 
+interface IRegisterPayload{
+  usename: string;
+  email: string;
+  password: string;
+
+}
 const Register = () => {
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+
+  const register = async (payload: IRegisterPayload) => {
+    try {
+      const { data } = await axios.post("http://localhost:3000/register", payload);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const mutationRegister = useMutation({
+    mutationFn: register,
+    onSuccess:() => {
+      navigate('/login')
+    }
+  })
+  const handleRegister = (values: IRegisterPayload) => {
+    mutationRegister.mutate(values);
+  };
   return (
     <>
-      <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <Card  variant="outlined" style={{ width: 400 }}>
-            <h1 style={{ textAlign: "center" }}>Register Form</h1>
-            <Form layout="vertical" >
-                <Form.Item label="username" rules={[{ required: true, message: "Please input your username!" }]}>
-                    <Input/>
-                </Form.Item>
-                <Form.Item label="email" rules={[{ required: true, message: "Please input your email!" }]}>
-                    <Input/>
-                </Form.Item>
-                <Form.Item label="password" rules={[{ required: true, message: "Please input your password!" }]}>
-                    <Input type="password"/>
-                </Form.Item>
-                <Form.Item  style={{ textAlign: "end" }}>
-                    <Button htmlType="submit" type="primary">Submit</Button>
-                </Form.Item>
-            </Form>
-          </Card>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Card variant="outlined" style={{ width: 400 }}>
+          <h1 style={{ textAlign: "center" }}>Register Form</h1>
+          <Form form={form} onFinish={handleRegister} layout="vertical">
+            <Form.Item
+              label="username"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="email"
+              name="email"
+              rules={[{ required: true, message: "Please input your email!" }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input type="password" />
+            </Form.Item>
+            <Form.Item style={{ textAlign: "end" }}>
+              <Button htmlType="submit" type="primary">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
       </div>
     </>
   );
